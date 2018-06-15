2e4b3bdf48b88959f302cdb416600dde03b4b62a
Georgia <giacovou44@gmail.com>
2018-06-15 17:47:07 +0100

    Map styling
    Added the map styling in the main.js. It is bad styling so needs changing

--- html/main.js
+++ html/main.js
@@ -24,14 +24,95 @@ $(document).ready(function () {
   
 });
 
-var sectionNames = Object.keys(secPlaces);
-var forLimit = sectionNames.length;
 
 function initMap() {
+    var sectionNames = Object.keys(secPlaces);
+    var forLimit = sectionNames.length;
     var polygons = [];
     map = new google.maps.Map(document.getElementById('googleMap'),{
         center: {lat: 51.5287718, lng: -0.2416808},
-        zoom: 9
+        zoom: 9,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+
     });
 
     /*
@@ -61,7 +142,9 @@ function initMap() {
             strokeWeight: 3
             })
         );
-    polygons[polygons.length -1].setMap(map);
+        polygons[polygons.length -1].setMap(map);
     }
+    
+    
 }
 
